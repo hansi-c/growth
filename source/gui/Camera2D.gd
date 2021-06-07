84eae3204d14976d1fd4b0f2a7abb37be96f86e6
rr <rdssdf.fd@gmx.at>
2021-06-07 15:24:40 +0200

    try to make using ui elements, like sliding a slider or scrollbar not also pan the camera. no success so far

--- source/gui/Camera2D.gd
+++ source/gui/Camera2D.gd
@@ -3,6 +3,5 @@ extends Camera2D
 func _on_ZoomScrollBar_value_changed(value):
 	zoom = Vector2(value, value)
 
-
 func _on_camera_panning(relative):
 	position -= relative
--- source/gui/CameraPanning.gd
+++ source/gui/CameraPanning.gd
@@ -1,10 +1,12 @@
-extends Node2D
+extends Control
 
 var _dragging = false
+#var _receive_drag = true
 
 signal camera_panning(relative)
 
 func _input(event):
+#	print("unhandled input event: %s" % event.as_text())
 	if event is InputEventMouseButton:
 		_dragging = event.pressed
 	if _dragging and event is InputEventMouseMotion:
