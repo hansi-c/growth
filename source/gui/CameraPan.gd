48a05b6d08d2088e4d894616349c0c39797c6563
hansi-c <christoph.czurda@gmail.com>
2021-06-11 14:59:05 +0200

    fix panning problem in gui

--- source/gui/CameraPan.gd
+++ source/gui/CameraPan.gd
@@ -1,11 +1,12 @@
-extends Control
+extends Node2D
+
 
 var _dragging = false
 #var _receive_drag = true
 
 signal camera_panning(relative)
 
-func _input(event):
+func _unhandled_input(event):
 #	print("unhandled input event: %s" % event.as_text())
 	if event is InputEventMouseButton:
 		_dragging = event.pressed
