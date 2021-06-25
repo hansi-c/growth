84d60bb3193ae4dd17d190d7aff61a634c8c7270
christoph <christoph.czurda@gmail.com>
2021-06-25 13:43:57 +0200

    rename source files (camel case it is i guess..)

---
+++ source/render/TurtleConfig.gd
@@ -0,0 +1,17 @@
+class_name TurtleConfig
+
+var start_angle = -PI/2
+var turn_angle = PI/4
+var line_length = 10.0
+var width_falloff = 1.0
+
+var abilities = {}
+
+#add_ability("F", "draw_line")
+#add_ability("G", "draw_line")
+#add_ability("-", "turn_cw")
+#add_ability("+", "turn_ccw")
+#add_ability("[", "open_branch")
+#add_ability("]", "close_branch")
+#add_ability("A", "shape_1")
+#add_ability("B", "shape_2")
--- source/render/turtle_config.gd
+++
@@ -1,6 +0,0 @@
-class_name TurtleConfig
-
-var start_angle = -PI/2
-var turn_angle = PI/4
-var line_length = 10.0
-var width_falloff = 1.0
