fb4c57d46040e3d64ee40365ac0e529b6aa23e88
christoph <christoph.czurda@gmail.com>
2021-07-12 02:40:20 +0200

    remove print statements

--- source/gui/Camera2D.gd
+++ source/gui/Camera2D.gd
@@ -12,11 +12,9 @@ func _on_camera_reset():
 
 func _on_camera_zooming(zoom_change, mouse_pos):
 	_zoom_at_point(zoom_change, mouse_pos)
-	print ("signal detected")
 
 func _zoom_at_point(zoom_change, mouse_position):
 	zoom = zoom * zoom_change
-	print (zoom_change)
 	var delta_x = (mouse_position.x - global_position.x) * (zoom_change - 1)
 	var delta_y = (mouse_position.y - global_position.y) * (zoom_change - 1)
 	global_position.x = global_position.x - delta_x
