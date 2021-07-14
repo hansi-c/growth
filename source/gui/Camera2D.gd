c93f99b3e01db80f8977bb062719968e37e3125a
christoph <christoph.czurda@gmail.com>
2021-07-15 00:25:14 +0200

    reduce hardcoded node paths by sending focus_camera signal to main class first

--- source/gui/Camera2D.gd
+++ source/gui/Camera2D.gd
@@ -6,14 +6,11 @@ func _on_ZoomScrollBar_value_changed(value):
 func _on_camera_panning(relative):
 	position -= relative * zoom
 
-func _on_camera_reset():
-	position = get_node("../PlantCanvas/GrowthStartPosition").position
+func _on_focus_camera(point: Vector2):
+	position = point
 	zoom = Vector2(1,1)
 
-func _on_camera_zooming(zoom_change, mouse_pos):
-	_zoom_at_point(zoom_change, mouse_pos)
-
-func _zoom_at_point(zoom_change, mouse_position):
+func _on_camera_zooming(zoom_change, mouse_position):
 	zoom = zoom * zoom_change
 	var delta_x = (mouse_position.x - global_position.x) * (zoom_change - 1)
 	var delta_y = (mouse_position.y - global_position.y) * (zoom_change - 1)
