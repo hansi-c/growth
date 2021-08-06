36d4f15a4572c7ec8049ba316f2f4ccf30731ddd
christoph <christoph.czurda@gmail.com>
2021-08-07 01:42:13 +0200

    improve access to global rng state

--- source/scenes/global/Globals.gd
+++ source/scenes/global/Globals.gd
@@ -4,5 +4,5 @@ var grammar: ILGrammar
 var turtle_settings: TurtleSettings
 var turtle_abilities: TurtleAbilities
 var turtle_potential_abilities: Array
-var rng_state: RngState = RngState.new()
+var rng_state: RngState
 var last_editor_tab: int = 0
