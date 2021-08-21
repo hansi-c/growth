679da6ff18b930650bd09efc2bb92b9f6cf2a41d
christoph <christoph.czurda@gmail.com>
2021-08-21 15:31:15 +0200

    add license header to source files

--- source/scenes/simulation/gui/CameraPan.gd
+++ source/scenes/simulation/gui/CameraPan.gd
@@ -1,13 +1,27 @@
+# growth - Lindenmayer Systems implemented in Godot
+# Copyright (C) 2021  Christoph Czurda <rdssdf.fd@gmx.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 extends Node2D
 
 
 var _dragging = false
-#var _receive_drag = true
 
 signal camera_panning(relative)
 
 func _unhandled_input(event):
-#	print("unhandled input event: %s" % event.as_text())
 	if event is InputEventMouseButton:
 		_dragging = event.pressed
 	if _dragging and event is InputEventMouseMotion:
