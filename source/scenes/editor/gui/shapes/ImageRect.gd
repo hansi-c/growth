de526921ff054f5a72e0cca5344a1b8a2f9e2e6c
christoph <christoph.czurda@gmail.com>
2021-10-02 21:03:26 +0200

    delete commented code

--- source/scenes/editor/gui/shapes/ImageRect.gd
+++ source/scenes/editor/gui/shapes/ImageRect.gd
@@ -2,19 +2,12 @@ extends TextureRect
 
 var drag_handler = DragHandler.new()
 
+func _on_self_gui_input(event):
+	drag_handler.handle_drag(event, self)
+
 func is_inside_shape(position: Vector2) -> bool:
 	var image: Image = texture.get_data()
 	image.lock()
 	var color: Color = image.get_pixelv(position)
 	image.unlock()
-	print(image)
-	print("hi %s %s %s" % [str(name), position, color])
-	return color.a != 0
-
-#func _draw():
-#	var center = get_size()/2
-#	draw_circle(center, center.x, Color.black)
-
-
-func _on_self_gui_input(event):
-	drag_handler.handle_drag(event, self)
+	return color.a > 0.0
