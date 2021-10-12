5a085deb65552a07115b9418546dc6caf220fc3f
christoph <christoph.czurda@gmail.com>
2021-10-12 04:37:23 +0200

    some changes to shapes editor

--- source/render/shapes/RectangeConfiguration.gd
+++ source/render/shapes/RectangeConfiguration.gd
@@ -1,5 +1,5 @@
 extends ShapeConfiguration
-class_name CircleConfiguration
+class_name RectangleConfiguration
 
 func draw(target: CanvasItem, position: Vector2, _angle: float=0.0):
 	pass
--- source/scenes/editor/gui/shapes/DrawingNode.gd
+++ source/scenes/editor/gui/shapes/DrawingNode.gd
@@ -7,16 +7,22 @@ onready var rectangle_rect = $AttachmentRect/RectangleRect
 onready var texture_rect = $AttachmentRect/TextureRect
 var attachment_pos: Vector2
 var stem_pos: Vector2
+var _helper_line_end: Vector2
 
 func _process(_delta):
 	attachment_pos = attachment_node.get_position() \
 		+ attachment_node.get_pivot_offset()
 	stem_pos = stem_node.get_position()
+	_helper_line_end = stem_pos + Vector2(stem_pos.length(), 0)
 	update()
 
 func _draw():
 	var attachment_color: Color = attachment_node.get_frame_color()
 	draw_line(attachment_pos, stem_pos, attachment_color)
+	_draw_helper_line()
+
+func _draw_helper_line():
+	draw_line(stem_pos, _helper_line_end, attachment_node.get_frame_color())
 
 func _on_ShapeSourceButton_circle_selected():
 	circle_rect.show()
